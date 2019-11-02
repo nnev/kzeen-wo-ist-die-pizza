924ea27c45dd9b76590c5815c3cdd27e00035414
Stefan Breunig <stefan-github@yrden.de>
2019-11-02 18:34:02 +0100

    do not download from remote host for specs
    This is a change in behaviour for the acceptance tests. We can
    also rm -rf the spec/fixtures/autogen/ folder to rerun them if
    something breaks.

--- spec/features/ordering_spec.rb
+++ spec/features/ordering_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 feature 'ordering process', js: true do
+  before do
+    allow(JsonCache).to receive(:get) do |path|
+      full_path = File.join(__dir__, "../fixtures/autogen", path)
+
+      if File.exist?(full_path)
+        next JSON.parse(File.read(full_path))
+      end
+
+      raise "not stubbed: #{path}\nIf you changed something, you might need to remove this line to allow recording new fixtures. They should be committed to the repo."
+
+      url = Rails.application.config.shop_url + path
+      raw = ::Net::HTTP.get(URI.parse(url))
+      FileUtils.mkdir_p(File.dirname(full_path))
+      File.write(full_path, raw)
+
+      JSON.parse(raw)
+    end
+  end
+
   it 'allows me to add an order on desktop' do
     nick = 'capy1'
 
@@ -31,7 +50,7 @@ feature 'ordering process', js: true do
     # wait for basket to be updated
     expect(page).to have_css('#basket-sum', text: '9,40 €')
     order = Order.find_by(nick: nick)
-    expect(order.order).to eq [{"product_id"=>184487, "size"=>12956, "extra_ingred"=>[20, 246], "basic_ingred"=>{}}]
+    expect(order.order).to eq [{product_id: 184487, size: 12956, extra_ingred: [20, 246], basic_ingred: {}}]
 
     # go back to basket
     sleep 1
